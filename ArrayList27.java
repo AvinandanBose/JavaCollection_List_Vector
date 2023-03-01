f3e2fbd6a38a04a686c38788141e79471b767162
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-02 01:39:43 +0530

    Add files via upload

---
+++ ArrayList27.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayList27 {
+
+    public static void main(String[] args) {
+        // subList?(int fromIndex, int toIndex)
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Hello");
+        arrayList.add("World");
+        arrayList.add("!");
+        System.out.println(arrayList);
+        List<String> arr = arrayList.subList(0, 2);
+         System.out.println(arr);
+    }
+    
+}
