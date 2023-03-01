8a0b0a7600da605050593752d9adf47039d7f51d
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-01 18:35:53 +0530

    Add files via upload

---
+++ ArrayList5.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayList5 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        System.out.println(arrayList);
+    }
+    
+}
