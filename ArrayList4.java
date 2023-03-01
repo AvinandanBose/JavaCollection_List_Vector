6daff925225b77742af5229c9142131d924fa6f4
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-01 15:14:58 +0530

    Add files via upload

--- ArrayList4.java
+++ ArrayList4.java
@@ -8,6 +8,15 @@ public class ArrayList4 {
         arrayList.add(0,"Hello");
         arrayList.add(1,"World");
         arrayList.add(2,"!");
+
+        System.out.println(arrayList);
+        
+        for (int i = 0; i < arrayList.size(); i++) {
+            if(i==1){
+                arrayList.add(i,"world");
+            }
+            
+        }
         System.out.println(arrayList);
     }
     
