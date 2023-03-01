7ae167e10aaebf19808a248cdb0465c38d847ecc
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-01 15:09:00 +0530

    Add files via upload

---
+++ ArrayList4.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class ArrayList4 {
+
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(0,"Hello");
+        arrayList.add(1,"World");
+        arrayList.add(2,"!");
+        System.out.println(arrayList);
+    }
+    
+}
