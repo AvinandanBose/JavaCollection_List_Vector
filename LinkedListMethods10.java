003c43de6a94428051445c06f9ffcc8d0d3db6df
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-07 03:15:00 +0530

    Add files via upload

---
+++ LinkedListMethods10.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class LinkedListMethods10 {
+    public static void main(String[] args) {
+        // peekFirst()
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list);
+
+        Integer i = list.peekFirst();
+
+        System.out.println(i);
+        System.out.println(list);
+
+    }
+    
+}
---
+++ LinkedListMethods11.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class LinkedListMethods11 {
+
+    public static void main(String[] args) {
+        // peekLast()
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list);
+
+        Integer i = list.peekLast();
+        System.out.println(i);
+        System.out.println(list);
+
+    }
+    
+}
