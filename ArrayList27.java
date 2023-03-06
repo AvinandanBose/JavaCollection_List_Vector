9a0831393f3731fad3bffa32afdc344778f058d1
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-07 01:31:26 +0530

    Add files via upload

--- ArrayList27.java
+++ ArrayList27.java
@@ -16,6 +16,21 @@ public class ArrayList27 {
          AbstractList<String> arr1 = (AbstractList<String>) arrayList.subList(0, 2);
 
             System.out.println(arr1);
+
+
+            ArrayList  arrayList1 = new ArrayList();
+
+            arrayList1.add("Hello");
+            arrayList1.add(28.9f);
+
+            System.out.println(arrayList1);
+            List arr2 =  arrayList1.subList(0, 1);
+
+            System.out.println(arr2);
+
+            AbstractList arr3 = (AbstractList) arrayList1.subList(0, 1);
+
+            System.out.println(arr3);
     }
     
 }
