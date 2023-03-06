03d43b24947acc1a28b4378dbf59c606b6942319
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-06 10:18:05 +0530

    Add files via upload

--- LinkedListConstructor1.java
+++ LinkedListConstructor1.java
@@ -3,11 +3,20 @@ import java.util.LinkedList;
 public class LinkedListConstructor1 {
 
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<String>();
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("Hello");
+        list1.add("World");
+        list1.add("!");
+
+        System.out.println(list1);
+
+
+
+        LinkedList list = new LinkedList();
 
         list.add("Hello");
-        list.add("World");
-        list.add("!");
+        list.add(2);
+        list.add(23.89F);
         System.out.println(list);
 
         list.add(1, "world");
