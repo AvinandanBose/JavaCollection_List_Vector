15370cca91c6cd341acb8872876f5888425089df
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-06 10:23:44 +0530

    Add files via upload

--- LinkedListConstructor2.java
+++ LinkedListConstructor2.java
@@ -9,6 +9,13 @@ public class LinkedListConstructor2 {
         collection.add("!");
         LinkedList<String> list = new LinkedList<String>(collection);
         System.out.println(list);
+        
+        Collection collection1 = new ArrayList();
+        collection1.add("Hello");
+        collection1.add(2.9);
+        collection1.add(3.98f);
+        LinkedList list1 = new LinkedList(collection1);
+        System.out.println(list1);
     
     }
     
