dd39d7513bceb9c2ae260baaece665aae2030082
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-07 23:39:23 +0530

    Add files via upload

---
+++ SynchronizationofArrayList.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class SynchronizationofArrayList {
+
+    public static void main(String[] args) {
+
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        List<Integer> list1 = Collections.synchronizedList(new ArrayList<>());
+        synchronized (list) {
+
+            list.add("A");
+            list.add("B");
+            list.add("C");
+            list.add("D");
+            list.add("E");
+            list.add("F");
+
+            list1.add(1);
+            list1.add(2);
+            list1.add(3);
+            list1.add(4);
+            list1.add(5);
+
+            System.out.println("List: " + list);
+            System.out.println("List1: " + list1);
+
+            list.remove("A");
+            list.remove("B");
+
+            list1.remove(1);
+            list1.remove(2);
+
+            System.out.println("List: " + list);
+            System.out.println("List1: " + list1);
+
+            Iterable<String> iterable = list;
+            for (String s : iterable) {
+                System.out.println(s);
+            }
+
+            Iterator<Integer> iterator = list1.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        }
+    }
+
+}
