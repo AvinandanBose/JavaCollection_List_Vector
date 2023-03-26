016509252857a1768e27d57ef2736c02ed18c9f2
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-26 23:30:40 +0530

    Add files via upload

---
+++ vectorconst1.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+public class vectorconst1 {
+    public static void main(String[] args) {
+
+        Vector<Integer> vector = new Vector<>(8);
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+        vector.add(5);
+        vector.add(6);
+        vector.add(7);
+        vector.add(8);
+        System.out.println(vector);
+    }
+}
