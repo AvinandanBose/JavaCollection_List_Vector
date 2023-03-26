32d617522898f6b5401ed4d1801ae6d2f0c426d5
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-26 23:30:15 +0530

    Add files via upload

---
+++ vectorconst.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class vectorconst {
+
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Hello");
+        vector.add("World");
+        vector.add("!");
+        System.out.println(vector);
+    }
+
+
+    
+}
