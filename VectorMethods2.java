ee7843c0b14389d6b9c71b42c59254f21eab42c2
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-27 22:01:52 +0530

    Add files via upload

---
+++ VectorMethods2.java
@@ -0,0 +1,23 @@
+import java.util.Vector;
+
+public class VectorMethods2 {
+
+    public static void main(String[] args){
+
+        // addElement(E obj)
+
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        vector.addElement(3);
+        vector.addElement(4);
+
+        System.out.println(vector);
+
+    }
+
+
+
+
+    
+}
