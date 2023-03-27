367512cf640005b728b2d8fb842442ef3d9a5d8a
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-27 22:20:55 +0530

    Add files via upload

---
+++ VectorMethods9.java
@@ -0,0 +1,21 @@
+
+import java.util.Vector;
+public class VectorMethods9 {
+
+    // insertElementAt(E obj, int index)
+
+    public static void main(String[] args){
+
+        Vector<Integer> vector = new Vector<>();
+        vector.addElement(1);
+        vector.addElement(2);
+        vector.addElement(3);
+        vector.addElement(4);
+        System.out.println(vector);
+        vector.insertElementAt(5, 2);
+
+        System.out.println(vector);
+
+    }
+    
+}
